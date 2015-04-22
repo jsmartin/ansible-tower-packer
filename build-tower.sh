d8d9d9bfc03581e8732266cd22fcd3646d7f8432
James Martin <jmartin@ansible.com>
2015-04-22 15:13:55 -0400

    now specify ansible version.

--- build-tower.sh
+++ build-tower.sh
@@ -1,27 +1,21 @@
 #! /bin/bash -ex
 
-if [ "$#" -ne 1 ]
+if [ "$#" -ne 2 ]
 then
-  echo "Usage: $(basename $0) tower-version"
-  echo "Example: $(basename $0) 2.1.1"
+  echo "Usage: $(basename $0) tower-version ansible-version"
+  echo "Example: $(basename $0) 2.1.3 1.9.0.1"
   exit 1
 fi
 
 tower_version=$1
+ansible_version=$2
 setup_dir=/tmp/ansible-tower-setup
+tower_box=tower-${tower_version}_ansible-${ansible_version}_virtualbox.box
 
-
-# tower playbooks need to be fetched prior to vagrant up or vagrant
-# provisioner will complain that they don't exist
-wget -P /tmp/ -N  http://releases.ansible.com/ansible-tower/setup/ansible-tower-setup-${tower_version}.tar.gz
-
-rm -rf ${setup_dir}
-mkdir ${setup_dir}
-tar xvf /tmp/ansible-tower-setup-${tower_version}.tar.gz -C ${setup_dir} --strip-components=1
-
-cp tower_setup_conf.yml /tmp/ansible-tower-setup
 
 if [ ! -e "output-virtualbox-iso/centos7.0.ovf" ]; then
 	packer build packer-centos-7.json
 fi
-packer build -var "tower_version=$tower_version" packer-tower.json
+
+packer build -var "tower_version=$tower_version" -var "ansible_version=$ansible_version" packer-tower.json
+
