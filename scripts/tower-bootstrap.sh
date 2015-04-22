803e9577b03eff2f75dd1e75d71b015ce99571a5
James Martin <jmartin@ansible.com>
2015-04-22 13:36:13 -0400

    fetching tower install from inside vm.

--- scripts/tower-bootstrap.sh
+++ scripts/tower-bootstrap.sh
@@ -1,2 +1,2 @@
 yum -y install python-devel gcc http://mirror.pnl.gov/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-yum -y install python-pip python-crypto ansible
+yum -y install python-pip python-crypto ansible-$ansible_version
